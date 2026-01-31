0184e792660ca5712ca38dafa917846ff392e4f4
Sodel <delaude.so@gmail.com>
2026-01-31 13:57:17 +0100

    Initial commit - Spring Boot project setup

---
+++ src/main/java/split/io/splitapi/SplitApiApplication.java
@@ -0,0 +1,13 @@
+package split.io.splitapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SplitApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SplitApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/split/io/splitapi/SplitApiApplicationTests.java
@@ -0,0 +1,13 @@
+package split.io.splitapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SplitApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
