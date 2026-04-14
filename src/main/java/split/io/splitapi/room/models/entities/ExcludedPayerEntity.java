351a976ae66ae157a5f7266c540db74eb4b82391
Sodel <delaude.so@gmail.com>
2026-04-14 23:02:04 +0200

    Add ExcludedPayerId class and update ExcludedPayerEntity to use composite key

--- src/main/java/split/io/splitapi/room/models/entities/ExcludedPayerEntity.java
+++ src/main/java/split/io/splitapi/room/models/entities/ExcludedPayerEntity.java
@@ -1,18 +1,19 @@
 package split.io.splitapi.room.models.entities;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
 import lombok.Getter;
 
 @Getter
 @Entity
+@IdClass(ExcludedPayerId.class)
 @Table(name = "excluded_payers")
 public class ExcludedPayerEntity {
 
+    @Id
     @Column(name = "expense_id")
     private String expenseId;
 
+    @Id
     @Column(name = "payer_id")
     private String payerId;
 
