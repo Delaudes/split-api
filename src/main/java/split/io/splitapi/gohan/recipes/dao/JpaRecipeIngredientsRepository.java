5f4c33cc60ca731768e89afdd7a28e4ce7cc7c5b
Sodel <delaude.so@gmail.com>
2026-08-29 15:00:53 +0200

    Add transaction management to deleteRecipeIngredient and enhance saveRecipeIngredient with ingredient validation

--- src/main/java/split/io/splitapi/gohan/recipes/dao/JpaRecipeIngredientsRepository.java
+++ src/main/java/split/io/splitapi/gohan/recipes/dao/JpaRecipeIngredientsRepository.java
@@ -1,7 +1,12 @@
 package split.io.splitapi.gohan.recipes.dao;
 
 import org.springframework.data.repository.Repository;
+import org.springframework.transaction.annotation.Transactional;
 import split.io.splitapi.gohan.recipes.models.entities.RecipeIngredientEntity;
 
 public interface JpaRecipeIngredientsRepository extends Repository<RecipeIngredientEntity, String>, RecipeIngredientsRepository {
+
+    @Override
+    @Transactional
+    void deleteByRecipeIdAndIngredientId(String recipeId, String ingredientId);
 }
